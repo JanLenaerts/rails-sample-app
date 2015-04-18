947e17090bb25527d4898f44600bb0fc007db4e7
Jan Lenaerts <hello@janlenaerts.be>
2015-04-18 15:39:01 +0200

    Make a basic User model (including secure passwords)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
+
+	test "should get new" do
     get :new
     assert_response :success
+	assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
